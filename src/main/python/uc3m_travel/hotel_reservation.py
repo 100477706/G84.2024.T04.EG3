3f9b6e15a8e74702263eda2f04af5ad75df790a3
100477706 <100477706@alumnos.uc3m.es>
2024-04-17 11:43:14 +0200

    Version 19: Copiamos el método de validación del DNI de hotel_manager.py a hotel_reservation.py

--- src/main/python/uc3m_travel/hotel_reservation.py
+++ src/main/python/uc3m_travel/hotel_reservation.py
@@ -134,13 +134,13 @@ class HotelReservation:
     @staticmethod
     def validate_dni(dni):
         """RETURN TRUE IF THE DNI IS RIGHT, OR FALSE IN OTHER CASE"""
-        c = {"0": "T", "1": "R", "2": "W", "3": "A", "4": "G", "5": "M",
-             "6": "Y", "7": "F", "8": "P", "9": "D", "10": "X", "11": "B",
-             "12": "N", "13": "J", "14": "Z", "15": "S", "16": "Q", "17": "V",
-             "18": "H", "19": "L", "20": "C", "21": "K", "22": "E"}
-        v = int(dni[0:8])
-        r = str(v % 23)
-        return dni[8] == c[r]
+        characters = {"0": "T", "1": "R", "2": "W", "3": "A", "4": "G", "5": "M",
+                      "6": "Y", "7": "F", "8": "P", "9": "D", "10": "X", "11": "B",
+                      "12": "N", "13": "J", "14": "Z", "15": "S", "16": "Q", "17": "V",
+                      "18": "H", "19": "L", "20": "C", "21": "K", "22": "E"}
+        digits = int(dni[0:8])
+        letter = str(digits % 23)
+        return dni[8] == characters[letter]
     def validate_name_surname(self, guest_name):
         r = r"^(?=^.{10,50}$)([a-zA-Z]+(\s[a-zA-Z]+)+)$"
         myregex = re.compile(r)
