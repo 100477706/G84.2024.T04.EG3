5ba05aec89e34179de3a52c7d5672fb498fd662d
100477706 <100477706@alumnos.uc3m.es>
2024-04-17 11:38:59 +0200

    Version 18: Copiamos el método de validación del DNI de hotel_manager.py a atrribute_idcard.py

--- src/main/python/uc3m_travel/attribute/attribute_idcard.py
+++ src/main/python/uc3m_travel/attribute/attribute_idcard.py
@@ -14,10 +14,10 @@ class IdCard(Attribute):
 
     def validate_dni(self, dni):
         """RETURN TRUE IF THE DNI IS RIGHT, OR FALSE IN OTHER CASE"""
-        c = {"0": "T", "1": "R", "2": "W", "3": "A", "4": "G", "5": "M",
-             "6": "Y", "7": "F", "8": "P", "9": "D", "10": "X", "11": "B",
-             "12": "N", "13": "J", "14": "Z", "15": "S", "16": "Q", "17": "V",
-             "18": "H", "19": "L", "20": "C", "21": "K", "22": "E"}
-        v = int(dni[0:8])
-        r = str(v % 23)
-        return dni[8] == c[r]
+        characters = {"0": "T", "1": "R", "2": "W", "3": "A", "4": "G", "5": "M",
+                      "6": "Y", "7": "F", "8": "P", "9": "D", "10": "X", "11": "B",
+                      "12": "N", "13": "J", "14": "Z", "15": "S", "16": "Q", "17": "V",
+                      "18": "H", "19": "L", "20": "C", "21": "K", "22": "E"}
+        digits = int(dni[0:8])
+        letter = str(digits % 23)
+        return dni[8] == characters[letter]
