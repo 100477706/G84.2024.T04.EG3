03876481c378057d8ba3353041e0f8220a6c082d
100477706 <100477706@alumnos.uc3m.es>
2024-04-17 12:31:44 +0200

    Version 31: Copiamos el método validate_idcard y validate_dni a hotel_stay.py.

--- src/main/python/uc3m_travel/hotel_stay.py
+++ src/main/python/uc3m_travel/hotel_stay.py
@@ -1,6 +1,9 @@
 ''' Class HotelStay (GE2.2) '''
 from datetime import datetime
 import hashlib
+import re
+from uc3m_travel.hotel_management_exception import HotelManagementException
+
 
 class HotelStay():
     """Class for representing hotel stays"""
@@ -64,3 +67,23 @@ class HotelStay():
     def departure(self, value):
         """returns the value of the departure date"""
         self.__departure = value
+
+
+    def validate_idcard(self, my_id_card):
+        configuracion = r'^[0-9]{8}[A-Z]{1}$'
+        my_regex = re.compile(configuracion)
+        if not my_regex.fullmatch(my_id_card):
+            raise HotelManagementException("Invalid IdCard format")
+        if not self.validate_dni(my_id_card):
+            raise HotelManagementException("Invalid IdCard letter")
+        return my_id_card
+    @staticmethod
+    def validate_dni(dni):
+        """RETURN TRUE IF THE DNI IS RIGHT, OR FALSE IN OTHER CASE"""
+        characters = {"0": "T", "1": "R", "2": "W", "3": "A", "4": "G", "5": "M",
+                      "6": "Y", "7": "F", "8": "P", "9": "D", "10": "X", "11": "B",
+                      "12": "N", "13": "J", "14": "Z", "15": "S", "16": "Q", "17": "V",
+                      "18": "H", "19": "L", "20": "C", "21": "K", "22": "E"}
+        digits = int(dni[0:8])
+        letter = str(digits % 23)
+        return dni[8] == characters[letter]
