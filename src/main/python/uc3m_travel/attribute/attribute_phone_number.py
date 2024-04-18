c6bf86f8a8e18145fd456bc7fd3bea1fed9fe7fc
100477706 <100477706@alumnos.uc3m.es>
2024-04-18 11:59:24 +0200

    Version 49.2: Arreglo de la clase attribute PhoneNumber

--- src/main/python/uc3m_travel/attribute/attribute_phone_number.py
+++ src/main/python/uc3m_travel/attribute/attribute_phone_number.py
@@ -4,6 +4,6 @@ import re
 
 class PhoneNumber(Attribute):
     def __init__(self, attr_value):
-        self.__regex_pattern = r"^(\+)[0-9]{9}"
-        self.__error_message = "Invalid name format"
-        self.__attr_value = self._validate(attr_value)
+        self._regex_pattern = r"^(\+)[0-9]{9}"
+        self._error_message = "Invalid name format"
+        self._attr_value = self._validate(attr_value)
