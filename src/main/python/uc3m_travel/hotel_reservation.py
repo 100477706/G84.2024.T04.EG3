09cc36c50679b6ae06ad2495c839b7576fd060a2
100477706 <100477706@alumnos.uc3m.es>
2024-04-18 12:16:15 +0200

    Version 52: Borrado de la validación del día de llegada en hotel_reservation.py

--- src/main/python/uc3m_travel/hotel_reservation.py
+++ src/main/python/uc3m_travel/hotel_reservation.py
@@ -6,6 +6,7 @@ from uc3m_travel.hotel_management_exception import HotelManagementException
 from uc3m_travel.attribute.attribute_idcard import IdCard
 from uc3m_travel.attribute.attribute_name_surname import NameSurname
 from uc3m_travel.attribute.attribute_phone_number import PhoneNumber
+from uc3m_travel.attribute.attribute_arrival_date import ArrivalDate
 
 class HotelReservation:
     """Class for representing hotel reservations"""
@@ -22,7 +23,7 @@ class HotelReservation:
         self.__credit_card_number = self.validatecreditcard(credit_card_number)
         self.__id_card = IdCard(id_card).value
         justnow = datetime.utcnow()
-        self.__arrival = self.validate_arrival_date(arrival)
+        self.__arrival = ArrivalDate(arrival).value
         self.__reservation_date = datetime.timestamp(justnow)
         self.__name_surname = NameSurname(name_surname).value
         self.__phone_number = PhoneNumber(phone_number).value
