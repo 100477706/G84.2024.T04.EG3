a5b81285f708c8f273476e9835e23464182cce57
Joaquín Pujol Carrillo <100495826@gmail.com>
2024-04-25 12:38:30 +0200

    Versión 93: creado is_today_departure dentro de hotel_departure

--- src/main/python/uc3m_travel/hotel_departure.py
+++ src/main/python/uc3m_travel/hotel_departure.py
@@ -3,8 +3,15 @@ Creado por Joaquín Pujol Carrillo in abr 2024
 Universidad Carlos III de Madrid
 """
 from datetime import datetime
+from uc3m_travel.hotel_manager_exception import HotelManagementException
 
 class HotelDeparture:
     def __init__(self, room_key):
         self.__room_key = room_key
         self.__checkout_time = datetime.timestamp(datetime.utcnow())
+
+    # Comprueba si hoy es el día de salida
+    def is_today_departure(self, departure_date_timestamp):
+        today = datetime.utcnow().date()
+        if datetime.fromtimestamp(departure_date_timestamp).date() != today:
+            raise HotelManagementException("Error: today is not the departure day")
