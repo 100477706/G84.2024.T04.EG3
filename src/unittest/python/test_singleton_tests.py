000211e79bb275d51f105649552cd59891315bef
Joaquín Pujol Carrillo <100495826@gmail.com>
2024-04-26 18:24:49 +0200

    Versión 15': hecho test singleton storecheckin

--- src/unittest/python/test_singleton_tests.py
+++ src/unittest/python/test_singleton_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from uc3m_travel import HotelManager
+from uc3m_travel.store.json_store_check_in import JsonStoreGuestArrival
 from uc3m_travel.store.json_store_check_out import JsonStoreCheckOut
 from uc3m_travel.store.json_store_reservation import JsonStoreReservation
 
@@ -18,6 +19,11 @@ class MyTestCase(unittest.TestCase):
     def test_singleton3(self):
         mi_primera_instancia = JsonStoreCheckOut()
         mi_segunda_instancia = JsonStoreCheckOut()
+        self.assertEqual(mi_primera_instancia, mi_segunda_instancia)
+
+    def test_singleton4(self):
+        mi_primera_instancia = JsonStoreGuestArrival()
+        mi_segunda_instancia = JsonStoreGuestArrival()
         self.assertEqual(mi_primera_instancia, mi_segunda_instancia)
 
 
