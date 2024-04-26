8dbc1955fd1ec365e0a0e02a0e88a5404a370c4e
Joaquín Pujol Carrillo <100495826@gmail.com>
2024-04-26 12:42:22 +0200

    Versión 133: singleton hecho en json_store_check_out SIGUE TENIENDO ERROR DEL COMMIT ANTERIOR DE GABRIEL

--- src/main/python/uc3m_travel/store/json_store_check_out.py
+++ src/main/python/uc3m_travel/store/json_store_check_out.py
@@ -5,25 +5,32 @@ from uc3m_travel.hotel_management_exception import HotelManagementException
 from uc3m_travel.store.json_store_father import JsonStoreFather
 
 class JsonStoreCheckOut(JsonStoreFather):
-    _data_list = []
-    _file_name = JSON_FILES_PATH + "store_check_out_.json"
-    _error_message_find = "Guest is already out"
+    class __JsonStoreCheckOut(JsonStoreFather):
+        _data_list = []
+        _file_name = JSON_FILES_PATH + "store_check_out_.json"
+        _error_message_find = "Guest is already out"
 
-    def IsGuestOut(self, room_key_list,room_key):
-        for checkout in room_key_list:
-            if checkout["room_key"] == room_key:
-                raise HotelManagementException(self._error_message_find)
+        def IsGuestOut(self, room_key_list,room_key):
+            for checkout in room_key_list:
+                if checkout["room_key"] == room_key:
+                    raise HotelManagementException(self._error_message_find)
 
-        room_checkout = {"room_key":  room_key, "checkout_time":datetime.timestamp(datetime.utcnow())}
+            room_checkout = {"room_key":  room_key, "checkout_time":datetime.timestamp(datetime.utcnow())}
 
-        room_key_list.append(room_checkout)
+            room_key_list.append(room_checkout)
 
-    def find_in_list_checkout(self, room_key, room_key_list):
-        found = False
-        for item in room_key_list:
-            if room_key == item["_HotelStay__room_key"]:
-                departure_date_timestamp = item["_HotelStay__departure"]
-                found = True
-        if not found:
-            raise HotelManagementException("Error: room key not found")
-        return departure_date_timestamp
+        def find_in_list_checkout(self, room_key, room_key_list):
+            found = False
+            for item in room_key_list:
+                if room_key == item["_HotelStay__room_key"]:
+                    departure_date_timestamp = item["_HotelStay__departure"]
+                    found = True
+            if not found:
+                raise HotelManagementException("Error: room key not found")
+            return departure_date_timestamp
+
+    __instance = None
+    def __new__(cls):
+        if not JsonStoreCheckOut.__instance:
+            JsonStoreCheckOut.__instance = JsonStoreCheckOut.__JsonStoreCheckOut()
+        return JsonStoreCheckOut.__instance
