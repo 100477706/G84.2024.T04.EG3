536264cf34cef9cc1ece2cc245f373926b7de9a8
Joaquín Pujol Carrillo <100495826@gmail.com>
2024-04-26 18:20:15 +0200

    Versión 149: hecho test singleton storecheckout

--- src/unittest/python/test_singleton_tests.py
+++ src/unittest/python/test_singleton_tests.py
@@ -1,7 +1,8 @@
 import unittest
 from uc3m_travel import HotelManager
+from uc3m_travel.store.json_store_check_out import JsonStoreCheckOut
 from uc3m_travel.store.json_store_reservation import JsonStoreReservation
-## CUIDADO QUE APARECE EL BACKUP PORQUE HAY UNA FALLITA CON EL SINGLETON DE HOTEL RESERVATION
+
 
 class MyTestCase(unittest.TestCase):
     def test_singleton1(self):
@@ -12,6 +13,11 @@ class MyTestCase(unittest.TestCase):
     def test_singleton2(self):
         mi_primera_instancia = JsonStoreReservation()
         mi_segunda_instancia = JsonStoreReservation()
+        self.assertEqual(mi_primera_instancia, mi_segunda_instancia)
+
+    def test_singleton3(self):
+        mi_primera_instancia = JsonStoreCheckOut()
+        mi_segunda_instancia = JsonStoreCheckOut()
         self.assertEqual(mi_primera_instancia, mi_segunda_instancia)
 
 
